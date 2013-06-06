5c4f24d6032b8291d16fc7cf54b379bc61f8932f
Craig Porter <cp7@sanger.ac.uk>
2013-06-06 11:49:13 +0100

    removed normalise

--- lib/Bio/VertRes/Config/Pipelines/SpadesAssembly.pm
+++ lib/Bio/VertRes/Config/Pipelines/SpadesAssembly.pm
@@ -21,20 +21,6 @@ extends 'Bio::VertRes::Config::Pipelines::Assembly';
 has '_assembler'           => ( is => 'ro', isa => 'Str',  default => 'spades' );
 has '_assembler_exec'      => ( is => 'ro', isa => 'Str',  default => '/software/pathogen/external/apps/usr/bin/spades.py' );
 has '_optimiser_exec'      => ( is => 'ro', isa => 'Str',  default => '/software/pathogen/external/apps/usr/bin/spades.py' );
-has '_normalise'           => ( is => 'ro', isa => 'Bool', default => 1 );
-has '_max_lanes_to_search' => ( is => 'ro', isa => 'Int', default => 2000 );
-has '_limit'               => ( is => 'ro', isa => 'Int', default => 500 );
-
-
-override 'to_hash' => sub {
-    my ($self) = @_;
-    my $output_hash = super();
-
-    $output_hash->{data}{normalise}      = $self->_normalise;
-
-    return $output_hash;
-};
-
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
