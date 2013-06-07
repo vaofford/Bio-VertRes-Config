31025440a968c764d4048f233160b3ba0cbe299d
Craig Porter <cp7@sanger.ac.uk>
2013-06-07 13:13:59 +0100

    fixed helminth commandline

--- lib/Bio/VertRes/Config/CommandLine/HelminthRegisterAndQCStudy.pm
+++ lib/Bio/VertRes/Config/CommandLine/HelminthRegisterAndQCStudy.pm
@@ -1,4 +1,4 @@
-package Bio::VertRes::Config::CommandLine::EukaryotesRegisterAndQCStudy;
+package Bio::VertRes::Config::CommandLine::HelminthRegisterAndQCStudy;
 
 # ABSTRACT: Create config scripts to map helminths
 
@@ -9,39 +9,38 @@ Create config scripts to map helminths
 =cut
 
 use Moose;
-use Bio::VertRes::Config::Recipes::EukaryotesRegisterAndQCStudy;
+use Bio::VertRes::Config::Recipes::HelminthRegisterAndQCStudy;
 with 'Bio::VertRes::Config::CommandLine::ReferenceHandlingRole';
 extends 'Bio::VertRes::Config::CommandLine::RegisterAndQCStudy';
 
-has 'database'    => ( is => 'rw', isa => 'Str', default => 'pathogen_euk_track' );
+has 'database'    => ( is => 'rw', isa => 'Str', default => 'pathogen_helminth_track' );
 
 override 'register_and_qc_usage_text' => sub {
     my ($self) = @_;
     return <<USAGE;
-Usage: eukaryote_register_and_qc_study [options]
-Pipeline to register and QC a eukaryote study.
+Usage: helminth_register_and_qc_study [options]
+Pipeline to register and QC a helminth study.
 
 # Search for an available reference
-eukaryote_register_and_qc_study -a "Plasmodium"
+helminth_register_and_qc_study -a "Caenorhabditis"
 
 # Register and QC a study
-eukaryote_register_and_qc_study -t study -i 1234 -r "Plasmodium_falciparum_3D7_02April2012"
+helminth_register_and_qc_study -t study -i 1234 -r "Caenorhabditis_elegans_WS226"
 
 # Register and QC a single lane
-eukaryote_register_and_qc_study -t lane -i 1234_5#6 -r "Plasmodium_falciparum_3D7_02April2012"
+helminth_register_and_qc_study -t lane -i 1234_5#6 -r "Caenorhabditis_elegans_WS226"
 
 # Register and QC a file of lanes
-eukaryote_register_and_qc_study -t file -i file_of_lanes -r "Plasmodium_falciparum_3D7_02April2012"
+helminth_register_and_qc_study -t file -i file_of_lanes -r "Caenorhabditis_elegans_WS226"
 
 # Register and QC a single species in a study
-eukaryote_register_and_qc_study -t study -i 1234 -r "Plasmodium_falciparum_3D7_02April2012" -s "Plasmodium falciparum"
+helminth_register_and_qc_study -t study -i 1234 -r "Caenorhabditis_elegans_WS226" -s "Caenorhabditis elegans"
 
 # This help message
-register_and_qc_study -h
+helminth_register_and_qc_study -h
 
 USAGE
 };
-
 
 
 __PACKAGE__->meta->make_immutable;
