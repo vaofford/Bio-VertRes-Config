6be6f4ed9f81746b33223cf9ec04e38a724fb4a0
nds <nds@sanger.ac.uk>
2015-12-08 14:55:48 +0000

    modules to register pacbio study, assemble and annotate

---
+++ lib/Bio/VertRes/Config/Recipes/PacbioRegister.pm
@@ -0,0 +1,57 @@
+package Bio::VertRes::Config::Recipes::PacbioRegister;
+# ABSTRACT: Register a pacbio study (i.e. creates assembly and annotation config files)
+
+=head1 SYNOPSIS
+
+Register a pacbio study
+   use Bio::VertRes::Config::Recipes::PacbioRegister;
+
+   my $obj = Bio::VertRes::Config::Recipes::PacbioRegister->new(
+     database => 'abc',
+     limits => {project => ['Study ABC']},
+     reference => 'ABC',
+     reference_lookup_file => '/path/to/refs.index');
+   $obj->create;
+
+=cut
+
+use Moose;
+use Bio::VertRes::Config::RegisterStudy;
+extends 'Bio::VertRes::Config::Recipes::Common';
+with 'Bio::VertRes::Config::Recipes::Roles::PacbioRegisterStudy';
+#with 'Bio::VertRes::Config::Recipes::Roles::Reference';
+with 'Bio::VertRes::Config::Recipes::Roles::CreateGlobal';
+
+has 'assembler'                   => ( is => 'ro', isa => 'Str',  default => 'hgap' );
+has 'no_ass'                      => ( is => 'ro', isa => 'Bool', default => 0 );
+has '_pipeline_version'           => ( is => 'ro', isa => 'Str' );
+has '_kingdom'                    => ( is => 'ro', isa => 'Str',  default => "Bacteria" );
+has '_vrtrack_processed_flags'    => ( is => 'ro', isa => 'HashRef', default => sub {{ assembled => 0, stored => 1}} );
+has '_max_threads'         => ( is => 'ro', isa => 'Int',  default => 16 );
+has '_queue'			   => ( is => 'ro', isa => 'Str', default => 'normal');
+has '_genome_size'         => ( is => 'ro', isa => 'Int', default => 3000000 );
+has '_memory'         	   => ( is => 'ro', isa => 'Int', default => 100000 ); # for assembly bsub job
+has '_memory_in_mb'		   => ( is => 'ro', isa => 'Int', default => 4000 ); # for annotation
+has '_target_coverage'	   => ( is => 'ro', isa => 'Int', default => 30 );
+has '_no_bsub'			   => ( is => 'ro', isa => 'Bool', default => 1 );
+has 'circularise'		   => ( is => 'ro', isa => 'Bool', default => 0 );
+
+
+override '_pipeline_configs' => sub {
+    my ($self) = @_;
+    my @pipeline_configs;
+   
+    if($self->assembler eq 'hgap')
+    {
+        $self->add_hgap_assembly_config(\@pipeline_configs);
+    }
+ 
+    $self->add_bacteria_annotate_config(\@pipeline_configs);
+
+    return \@pipeline_configs;
+};
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
+
---
+++ lib/Bio/VertRes/Config/Recipes/Roles/PacbioRegisterStudy.pm
@@ -0,0 +1,90 @@
+package Bio::VertRes::Config::Recipes::Roles::PacbioRegisterStudy;
+# ABSTRACT: Moose Role for registering a pacbio study
+
+=head1 SYNOPSIS
+
+Moose Role for registering a pacbio study
+
+   with 'Bio::VertRes::Config::Recipes::Roles::PacbioRegisterStudy';
+
+=method create
+
+Hooks into the create method after the base method is run to register a study
+
+=method 
+
+=cut
+
+use Moose::Role;
+use Bio::VertRes::Config::Pipelines::HgapAssembly;
+use Bio::VertRes::Config::RegisterStudy;
+
+# Register all the studies passed in the project limits array
+after 'create' => sub {
+  my ($self) = @_;
+  
+  if(defined($self->limits->{project}))
+  {
+    for my $study_name ( @{$self->limits->{project}} )
+    {
+      my $pipeline = Bio::VertRes::Config::RegisterStudy->new(
+        database    => $self->database,
+        study_name  => $study_name,
+        config_base => $self->config_base
+      );
+      $pipeline->register_study_name();
+    }
+  }
+};
+
+sub add_hgap_assembly_config
+{
+  my ($self, $pipeline_configs_array) = @_;
+  push(
+      @{$pipeline_configs_array},
+      Bio::VertRes::Config::Pipelines::HgapAssembly->new(
+          database                       => $self->database,
+          database_connect_file          => $self->database_connect_file,
+          config_base                    => $self->config_base,
+          root_base                      => $self->root_base,
+          log_base                       => $self->log_base,
+          overwrite_existing_config_file => $self->overwrite_existing_config_file,
+          limits                         => $self->limits,
+          _queue						 => $self->_queue,
+          _memory						 => $self->_memory,
+          _target_coverage			     => $self->_target_coverage,
+          _no_bsub						 => $self->_no_bsub,
+          circularise					 => $self->circularise,
+ 
+      )
+  );
+  return ;
+}
+
+
+sub add_bacteria_annotate_config
+{
+  my ($self, $pipeline_configs_array) = @_;
+  push(
+      @{$pipeline_configs_array},
+      Bio::VertRes::Config::Pipelines::AnnotateAssembly->new(
+          database                       => $self->database,
+          database_connect_file          => $self->database_connect_file,
+          root_base                      => $self->root_base,
+          log_base                       => $self->log_base,
+          config_base                    => $self->config_base,
+          overwrite_existing_config_file => $self->overwrite_existing_config_file,
+          limits                         => $self->limits,
+          _kingdom                       => $self->_kingdom,
+          _assembler                     => $self->assembler,
+          _memory_in_mb					 => $self->_memory_in_mb,
+      )
+  );
+  return ;
+}
+
+
+no Moose;
+1;
+
+
